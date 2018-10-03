b469232ddac94d8b2aaa1bc19e2374e9dd48d1bd
José Luis Reynoso Rodríguez <kpepis@gmail.com>
2018-10-03 10:03:00 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -8,11 +8,26 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const chatName = "Chat app";
 
 app.use(express.static(publicPath));
 
 io.on("connection", socket => {
   console.log("New user connected.");
+
+  // Receiving person
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: `Welcome to ${chatName}`,
+    createdAt: new Date().getTime()
+  });
+
+  // Everyone but the connecting user
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
 
   socket.on("createMessage", msg => {
     console.log("createMessage", msg);
@@ -21,6 +36,11 @@ io.on("connection", socket => {
       text: msg.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => console.log("Client disconnected"));
